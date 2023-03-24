c940449b428d706ef4f815fe6e9848d389d39f88
bengubastug <115447066+BenguBastug@users.noreply.github.com>
2023-03-24 12:36:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,15 @@ link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
 df_cars
 
+weight_hp_corr = sns.scatterplot(data = df_cars,
+                                 x = 'weightlbs',
+                                 y = 'hp',
+                                 hue = 'year',
+                                 size = 'cylinders',
+                                 palette = 'coolwarm'
+                                )
+plt.title("Weight-Hp")
+st.pyplot(weight_hp_corr.figure)
 
 
 viz_correlation = sns.heatmap(df_cars.corr(),
@@ -21,12 +30,4 @@ st.pyplot(viz_correlation.figure)
 
 
 
-weight_hp_corr = sns.scatterplot(data = df_cars,
-                                 x = 'weightlbs',
-                                 y = 'hp',
-                                 hue = 'year',
-                                 size = 'cylinders',
-                                 palette = 'coolwarm'
-                                )
-plt.title("Weight-Hp")
-st.pyplot(weight_hp_corr.figure)
+
