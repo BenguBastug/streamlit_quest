d2ddfd34407b30ef558bf09a8248f6636900957b
bengubastug <115447066+BenguBastug@users.noreply.github.com>
2023-03-24 12:30:10 +0300

    adding 2nd plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
 df_cars
 
-df_cars.groupby('continent').mean()
+
 
 viz_correlation = sns.heatmap(df_cars.corr(),
                               center=0,
@@ -19,3 +19,14 @@ viz_correlation = sns.heatmap(df_cars.corr(),
 plt.title("HeatMap")
 st.pyplot(viz_correlation.figure)
 
+
+
+weight_hp_corr = sns.scatterplot(data = df_cars,
+                                 x = 'weightlbs',
+                                 y = 'hp',
+                                 hue = 'year',
+                                 size = 'cylinders',
+                                 palette = 'coolwarm')
+
+plt.title("Weight-Hp")
+st.pyplot(weight_hp_corr.figure)
