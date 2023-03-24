aa02d7252e5753ce427b95616935bc2f0da298d6
bengubastug <115447066+BenguBastug@users.noreply.github.com>
2023-03-24 12:26:52 +0300

    groupby continent table

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,12 @@ st.title("Let's explore cars!")
 st.write('And decide which one is better for us')
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df = pd.read_csv(link)
-df
+df_cars = pd.read_csv(link)
+df_cars
 
-viz_correlation = sns.heatmap(df.corr(),
+df_cars.groupby('continent').mean()
+
+viz_correlation = sns.heatmap(df_cars.corr(),
                               center=0,
                               cmap = sns.color_palette("vlag", as_cmap=True)
                              )
