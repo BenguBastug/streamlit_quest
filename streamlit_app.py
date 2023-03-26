52f442f1b15ca2bc4cff3cee74679d766f140bf7
bengubastug <115447066+BenguBastug@users.noreply.github.com>
2023-03-26 23:11:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,16 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 st.title("Let's explore cars!")
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
+
 cars_eu = df_cars.loc[df_cars['continent'] == 'Europe.']
 cars_us = df_cars.loc[df_cars['continent'] == 'US.']
 cars_jp = df_cars.loc[df_cars['continent'] == 'Japan.']
-continents = [' ','US','Europe','Japan','All Regions']
+
+continents = ['US','Europe','Japan','All Regions']
 dropdown = st.selectbox('Select a Region:', continents)
 st.write('**You have selected:**',dropdown)
 
